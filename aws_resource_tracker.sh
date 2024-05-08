d0f0ee7779c8da146df7c1c543226e6d5cb5daec
Immah <emmanuelkaringi@gmail.com>
2024-05-08 20:21:25 +0300

    Update aws_resource_tracker with info on creating a cronjob

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -31,3 +31,18 @@ aws lambda list-functions
 #List IAM Users
 echo "Print list of IAM Users"
 aws iam list-users
+
+#Steps to create a cron job to run this script every 24 hours:
+
+# Open terminal.
+# Type crontab -e and press Enter. This will open the cron jobs configuration file in the default text editor.
+# Add the following line to the end of the file: 0 0 * * * /path/to/script.sh
+
+#This cron job configuration runs the script at midnight (00:00) every day. The five fields in the cron job definition represent:
+
+# Minute (0)
+# Hour (0)
+# Day of the month (* means every day)
+# Month (* means every month)
+# Day of the week (* means every day of the week)
+# So 0 0 * * * means "at 00:00 on every day of every month and every day of the week".
